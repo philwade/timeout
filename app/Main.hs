5e1a63a307468da28a552b0dfe30a9ec7788451f
Phil Wade <pwade@campuslabs.com>
2018-04-09 14:36:27 -0400

    Add usage and permission checking

--- app/Main.hs
+++ app/Main.hs
@@ -3,18 +3,60 @@ module Main where
 import Prelude hiding (readFile)
 import System.IO.Strict (readFile)
 import System.Environment
+import System.Posix.Files
 import Lib
 
 hostsFile :: String
-hostsFile = "samplehosts"
+hostsFile = "/etc/hosts"
 
 main :: IO ()
 main = do
-        hostcontent <- readFile hostsFile
+        haveAccess <- fileAccess hostsFile False True False
         args <- getArgs
-        let newhosts = unlines $ updateHosts args (lines hostcontent)
-        putStrLn newhosts
-        writeFile hostsFile newhosts
+        case args of
+            ["help"] -> printUsage
+            ["-h"] -> printUsage
+            _ ->
+                if haveAccess then
+                        do
+                        hostcontent <- readFile hostsFile
+                        let newhosts = unlines $ updateHosts args (lines hostcontent)
+                        putStrLn newhosts
+                        writeFile hostsFile newhosts
+                else
+                    putStrLn $ "Please run with sudo so I can modify " ++ hostsFile
+
+printUsage :: IO ()
+printUsage =
+    do
+        putStrLn "timeout - Block yourself from unproductive websites"
+        putStrLn ""
+        putStrLn "Usage: timeout [COMMAND] [arguments]"
+        putStrLn ""
+        putStrLn "Available commands:"
+        putStrLn "out                                   Block configured websites (default without any command)"
+        putStrLn "out [sitename]                        Block [sitename] configured websites"
+        putStrLn "in                                    Unblock configured websites"
+        putStrLn "in [sitename] [sitename]...           Unblock [sitename] configured websites"
+        putStrLn "add [sitename] [urls]...              Create block entry named [sitename] composed of [urls]"
+        putStrLn ""
+        putStrLn ""
+        putStrLn "Examples:"
+        putStrLn ""
+        putStrLn "Add twitter to block list and block:"
+        putStrLn "  $ timeout add twitter twitter.com www.twitter.com"
+        putStrLn ""
+        putStrLn "Block all sites configured:"
+        putStrLn "  $ timeout"
+        putStrLn ""
+        putStrLn "Unblock all sites configured:"
+        putStrLn "  $ timeout in"
+        putStrLn ""
+        putStrLn "Unblock just twitter:"
+        putStrLn "  $ timeout in twitter"
+        putStrLn ""
+        putStrLn "Unblock twitter and facebook:"
+        putStrLn "  $ timeout in twitter facebook"
 
 updateHosts :: [String] -> HostFile -> HostFile
 updateHosts args hosts =
